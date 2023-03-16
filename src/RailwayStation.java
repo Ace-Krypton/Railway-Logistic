06ef53910c1ce29e445e68943c32dc13bbd126df
Ace-Krypton <ramizna@code.edu.az>
2023-03-16 21:06:29 +0100

    .

--- src/RailwayStation.java
+++ src/RailwayStation.java
@@ -1,21 +1,16 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RailwayStation {
+    public ArrayList<RailwayStation> stations = new ArrayList<>();
+    public ArrayList<Locomotive> locomotives = new ArrayList<>();
     private String name;
     private String location;
     private final Scanner scan = new Scanner(System.in);
     private String input;
 
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getLocation() {
-        return location;
     }
 
     public void setLocation(String location) {
@@ -23,12 +18,21 @@ public class RailwayStation {
     }
 
     public void createLocomotives(Locomotive locomotiveObjs) {
+        if (stations.isEmpty()) {
+            System.out.println("Please, first create the Railway Station");
+            return;
+        }
+
         System.out.print("Enter the name of the locomotive: ");
         input = scan.next();
         locomotiveObjs.setName(input);
 
         System.out.println("Choose the home railway station: ");
+        for (RailwayStation station : stations) {
 
+        }
+
+        locomotives.add(locomotiveObjs);
     }
 
     public void createStations(RailwayStation stationObj) {
@@ -39,5 +43,11 @@ public class RailwayStation {
         System.out.print("Enter the location of the station: ");
         input = scan.next();
         stationObj.setLocation(input);
+
+        stations.add(stationObj);
+    }
+
+    public String toString() {
+        return
     }
 }
